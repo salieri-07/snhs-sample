6b5f573b6ba6208284cfb3d70ea96a50d870f35a
salieri-07 <mnbvcxzesh0101@gmail.com>
2025-05-09 09:47:11 +0000

    나이에 따른 평가 파일

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,21 @@
 import streamlit as st
 
-st.title("ㅇㅅㅎ의 첫 번째 앱")
-st.info("안녕하세요 반갑습니다. 저는 ~입니다.")
-st.write("안녕하세요 반갑습니다. 저는 ~입니다.")
+st.title("ㅇㅅㅎ임")
+st.info("뭘 보노")
+st.write("알라라라라라랄ㄹㄻㄴㅇㅁㄴㅇ")
+st.markdown("---") 
+
+age = st.number_input("나이가 몇이노", min_value=0, max_value=120, step=1)
+
+if(age<=19 and age>=17):
+    st.write("공부나 해라.")
+    st.image("https://image.yes24.com/momo/TopCate88/MidCate08/8777166.jpg", use_container_width=True)
+
+if(age>19):
+    st.write("만수무강 하세요.")
+    st.image("https://cdn.imweb.me/upload/S202112291accec1828d10/c5544d56fee70.png", use_container_width=True)
+
 
 st.markdown("---") 
-st.warning("⚠️ 1000이상을 만드세요.")
+
+
